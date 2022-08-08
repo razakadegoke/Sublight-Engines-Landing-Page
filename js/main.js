adb45ef1cf949c5e8295d6661c1d9b3342473f92
razakadegoke <razakadegoke@outlook.fr>
2022-08-08 14:18:33 -0400

    adding hamburger menu

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById("icon").addEventListener("click", () => {
+    if (document.getElementById("nav").style.visibility === "hidden") {
+        document.getElementById("nav").style.visibility = "visible"
+    }else{
+        document.getElementById("nav").style.visibility = "hidden"
+    }
+})
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1100) {
+        document.getElementById("nav").style.visibility = "visible"
+    }else{
+        document.getElementById("nav").style.visibility = "hidden"
+    }
+})
